77f269b5c05f73c02b82450a95c2de188ce92252
bscsany <bs@csany-zeg.hu>
2018-10-19 21:02:23 +0200

    aa

---
+++ konzolx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konzolx
+{
+    class Program
+    {
+        Program()
+        {
+            Console.WriteLine("Juhéééé");
+        }
+        static void Main(string[] args)
+        {
+            new Program();
+            Console.ReadKey();
+        }
+    }
+}
